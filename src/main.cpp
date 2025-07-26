a19ab77996690d01ed056e5cde096609fa126454
ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>
2025-07-26 17:58:10 +0530

    Fix: on force terminate, log msg as data stored
    Signed-off-by: ChandanKhamitkar <khamitkarsaichandan1035@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <branchdb/db/database.h>
 #include <branchdb/cli/start_cli.h>
+#include <csignal>>
 
 using namespace std;
 using namespace chrono;
 
+void handleSigint(int signal) {
+    cout << endl << "Data stored to DISK successfully, Status:[OK]" << endl;
+}
+
 int main()
 {
+    signal(SIGINT, handleSigint);
     branchdb::Database db;
     cli::startCLI(db);
     return 0;
